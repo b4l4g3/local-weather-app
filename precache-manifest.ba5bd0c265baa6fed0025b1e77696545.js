0585d4a2d2aa17defbff8c67ff14bd407a149c64
Balazs Kosa <b4l4g3@gmail.com>
2018-12-14 23:51:23 +0100

    Updates

---
+++ precache-manifest.ba5bd0c265baa6fed0025b1e77696545.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = [
+  {
+    "revision": "55d9cdc1dee40e3f88465fc160a2d50a",
+    "url": "/local-weather-app/static/media/fog.55d9cdc1.jpg"
+  },
+  {
+    "revision": "e0efd86879c8aba08e16",
+    "url": "/local-weather-app/static/js/main.e0efd868.chunk.js"
+  },
+  {
+    "revision": "fe8db27c86d1acbc9759",
+    "url": "/local-weather-app/static/js/runtime~main.fe8db27c.js"
+  },
+  {
+    "revision": "9f0634a2c5f264d472a405a33e281afe",
+    "url": "/local-weather-app/static/media/clear-day.9f0634a2.svg"
+  },
+  {
+    "revision": "46824d41eca9042f045c065726e3c0e1",
+    "url": "/local-weather-app/static/media/clear-night.46824d41.svg"
+  },
+  {
+    "revision": "c268e266efd1724ce0999bdd6cca468b",
+    "url": "/local-weather-app/static/media/cloudy.c268e266.svg"
+  },
+  {
+    "revision": "f6866f92d19f42efbdc1cd52e0c2bc28",
+    "url": "/local-weather-app/static/media/fog.f6866f92.svg"
+  },
+  {
+    "revision": "9c120232aecdf15ef35dcd4dc11227c0",
+    "url": "/local-weather-app/static/media/partly-cloudy-day.9c120232.svg"
+  },
+  {
+    "revision": "a93465c716af212e0fbdf09a4c5dbaf5",
+    "url": "/local-weather-app/static/media/partly-cloudy-night.a93465c7.svg"
+  },
+  {
+    "revision": "4dcbd9618828a86e98cb8040dd9a53fd",
+    "url": "/local-weather-app/static/media/rain.4dcbd961.svg"
+  },
+  {
+    "revision": "c9a2d7123bb06253603cb9b4628a0454",
+    "url": "/local-weather-app/static/media/sleet.c9a2d712.svg"
+  },
+  {
+    "revision": "93140a7820afd7627a73667c0f8ff5f2",
+    "url": "/local-weather-app/static/media/snow.93140a78.svg"
+  },
+  {
+    "revision": "e18b61b1446792d9a1a189135dc01ff5",
+    "url": "/local-weather-app/static/media/wind.e18b61b1.svg"
+  },
+  {
+    "revision": "f1e52ab872d263363bf45d69b00811b0",
+    "url": "/local-weather-app/static/media/clear-day.f1e52ab8.jpg"
+  },
+  {
+    "revision": "99516ec90736882dded6a4ebb1215c9a",
+    "url": "/local-weather-app/static/media/cloudy.99516ec9.jpg"
+  },
+  {
+    "revision": "0f104073a9464490b5f40034d051f43f",
+    "url": "/local-weather-app/static/media/wind.0f104073.jpg"
+  },
+  {
+    "revision": "99516ec90736882dded6a4ebb1215c9a",
+    "url": "/local-weather-app/static/media/partly-cloudy-day.99516ec9.jpg"
+  },
+  {
+    "revision": "93d04a5fd14bbf0f02be65d747eac8b1",
+    "url": "/local-weather-app/static/media/rain.93d04a5f.jpg"
+  },
+  {
+    "revision": "93d04a5fd14bbf0f02be65d747eac8b1",
+    "url": "/local-weather-app/static/media/sleet.93d04a5f.jpg"
+  },
+  {
+    "revision": "bb0b1e6239d5900cce582402f190e586",
+    "url": "/local-weather-app/static/media/snow.bb0b1e62.jpg"
+  },
+  {
+    "revision": "9c2393c8db2e81ac68b9e8ad6de8edb6",
+    "url": "/local-weather-app/static/media/wind.9c2393c8.jpg"
+  },
+  {
+    "revision": "1c45fdd74c51810f274b5d5faca2d455",
+    "url": "/local-weather-app/static/media/adrian-154479-unsplash.1c45fdd7.jpg"
+  },
+  {
+    "revision": "5256e3d141c6db51dfc8609b02d168ff",
+    "url": "/local-weather-app/static/media/clear-night.5256e3d1.jpg"
+  },
+  {
+    "revision": "a1d2b3ffb07fffe006be64467b83fbc3",
+    "url": "/local-weather-app/static/media/cloudy.a1d2b3ff.jpg"
+  },
+  {
+    "revision": "a1d2b3ffb07fffe006be64467b83fbc3",
+    "url": "/local-weather-app/static/media/partly-cloudy-night.a1d2b3ff.jpg"
+  },
+  {
+    "revision": "ebb46ef8a6cc7e7da148841eed07fb92",
+    "url": "/local-weather-app/static/media/rain.ebb46ef8.jpg"
+  },
+  {
+    "revision": "ebb46ef8a6cc7e7da148841eed07fb92",
+    "url": "/local-weather-app/static/media/sleet.ebb46ef8.jpg"
+  },
+  {
+    "revision": "77d184ae46ac7687bbc1f28e5c6269a1",
+    "url": "/local-weather-app/static/media/snow.77d184ae.jpg"
+  },
+  {
+    "revision": "5f6a95f960b364cebd2e",
+    "url": "/local-weather-app/static/js/1.5f6a95f9.chunk.js"
+  },
+  {
+    "revision": "d6b23e77ff83646a04e4a466e80f68f6",
+    "url": "/local-weather-app/index.html"
+  }
+];
