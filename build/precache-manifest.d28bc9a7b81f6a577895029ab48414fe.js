3f64025c6cdbcf53813a36202894fde702a593ed
Balazs Kosa <b4l4g3@gmail.com>
2018-12-21 19:56:44 +0100

    First commit

---
+++ build/precache-manifest.d28bc9a7b81f6a577895029ab48414fe.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = [
+  {
+    "revision": "48baedb281c572f9790bb61d3ca5c8a5",
+    "url": "/local-weather-app/static/media/rain.48baedb2.jpg"
+  },
+  {
+    "revision": "30122080cc61f6f7b130",
+    "url": "/local-weather-app/static/js/main.30122080.chunk.js"
+  },
+  {
+    "revision": "fe8db27c86d1acbc9759",
+    "url": "/local-weather-app/static/js/runtime~main.fe8db27c.js"
+  },
+  {
+    "revision": "cc793497dd365e576bd35c30ff24b179",
+    "url": "/local-weather-app/static/media/clear-day.cc793497.jpg"
+  },
+  {
+    "revision": "0fb544038da3520d4bbd8a1bde2d5096",
+    "url": "/local-weather-app/static/media/cloudy.0fb54403.jpg"
+  },
+  {
+    "revision": "55d9cdc1dee40e3f88465fc160a2d50a",
+    "url": "/local-weather-app/static/media/fog.55d9cdc1.jpg"
+  },
+  {
+    "revision": "0fb544038da3520d4bbd8a1bde2d5096",
+    "url": "/local-weather-app/static/media/partly-cloudy-day.0fb54403.jpg"
+  },
+  {
+    "revision": "bffd5acf58c93688468b46761f1a46f2",
+    "url": "/local-weather-app/static/media/rain.bffd5acf.jpg"
+  },
+  {
+    "revision": "775dead8d427067855dc5e43dfc14952",
+    "url": "/local-weather-app/static/media/sleet.775dead8.jpg"
+  },
+  {
+    "revision": "4abd9405a00f990381ce68e0d9192319",
+    "url": "/local-weather-app/static/media/snow.4abd9405.jpg"
+  },
+  {
+    "revision": "0d4184c4cc9d74ebac4216583057807b",
+    "url": "/local-weather-app/static/media/wind.0d4184c4.jpg"
+  },
+  {
+    "revision": "1c45fdd74c51810f274b5d5faca2d455",
+    "url": "/local-weather-app/static/media/adrian-154479-unsplash.1c45fdd7.jpg"
+  },
+  {
+    "revision": "28e6ea44333720338ef731845853e69d",
+    "url": "/local-weather-app/static/media/clear-night.28e6ea44.jpg"
+  },
+  {
+    "revision": "a1d2b3ffb07fffe006be64467b83fbc3",
+    "url": "/local-weather-app/static/media/cloudy.a1d2b3ff.jpg"
+  },
+  {
+    "revision": "a1d2b3ffb07fffe006be64467b83fbc3",
+    "url": "/local-weather-app/static/media/partly-cloudy-night.a1d2b3ff.jpg"
+  },
+  {
+    "revision": "e18b61b1446792d9a1a189135dc01ff5",
+    "url": "/local-weather-app/static/media/wind.e18b61b1.svg"
+  },
+  {
+    "revision": "48baedb281c572f9790bb61d3ca5c8a5",
+    "url": "/local-weather-app/static/media/sleet.48baedb2.jpg"
+  },
+  {
+    "revision": "77d184ae46ac7687bbc1f28e5c6269a1",
+    "url": "/local-weather-app/static/media/snow.77d184ae.jpg"
+  },
+  {
+    "revision": "e63699f891cb809552be7069a544638a",
+    "url": "/local-weather-app/static/media/wind.e63699f8.jpg"
+  },
+  {
+    "revision": "9f0634a2c5f264d472a405a33e281afe",
+    "url": "/local-weather-app/static/media/clear-day.9f0634a2.svg"
+  },
+  {
+    "revision": "46824d41eca9042f045c065726e3c0e1",
+    "url": "/local-weather-app/static/media/clear-night.46824d41.svg"
+  },
+  {
+    "revision": "c268e266efd1724ce0999bdd6cca468b",
+    "url": "/local-weather-app/static/media/cloudy.c268e266.svg"
+  },
+  {
+    "revision": "f6866f92d19f42efbdc1cd52e0c2bc28",
+    "url": "/local-weather-app/static/media/fog.f6866f92.svg"
+  },
+  {
+    "revision": "9c120232aecdf15ef35dcd4dc11227c0",
+    "url": "/local-weather-app/static/media/partly-cloudy-day.9c120232.svg"
+  },
+  {
+    "revision": "a93465c716af212e0fbdf09a4c5dbaf5",
+    "url": "/local-weather-app/static/media/partly-cloudy-night.a93465c7.svg"
+  },
+  {
+    "revision": "4dcbd9618828a86e98cb8040dd9a53fd",
+    "url": "/local-weather-app/static/media/rain.4dcbd961.svg"
+  },
+  {
+    "revision": "c9a2d7123bb06253603cb9b4628a0454",
+    "url": "/local-weather-app/static/media/sleet.c9a2d712.svg"
+  },
+  {
+    "revision": "93140a7820afd7627a73667c0f8ff5f2",
+    "url": "/local-weather-app/static/media/snow.93140a78.svg"
+  },
+  {
+    "revision": "0135eea22987cded31a6",
+    "url": "/local-weather-app/static/js/1.0135eea2.chunk.js"
+  },
+  {
+    "revision": "358b9ecef760220bf093aa4f07ca08a0",
+    "url": "/local-weather-app/index.html"
+  }
+];
